adb311362283ded6695a53c5d4fb281744fcbabe
1434988286@qq.com <kisses20000222>
2021-06-04 22:25:47 +0800

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,8 @@
+public class Client {
+    public static void main(String[] args) {
+        Chef chef = new Chef();
+        ICutFruit cut = new MultiBlade();
+        chef.SetCutMethod(cut);
+        chef.CutFruit("橙子");
+    }
+}
